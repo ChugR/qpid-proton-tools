1f532a2cea062163ca5fb15e527c90516c73d767
Chuck Rolke <crolke@redhat.com>
2015-12-23 10:56:47 -0500

    add dispatch setup script

---
+++ dispatch/dispatch-setup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Chuck Rolke 2015
+
+#
+# Set up proton and qpid for a dispatch test run.
+#
+# Usage:
+#  > ./dispatch_setup.sh
+#  > ./dispatch_setup.sh xxx
+#
+# A non-blank arg 1 skips the build part and just sets the environment
+#
+# And then with the environment set up run dispatch in kdevelop:
+#  > kdevelop &
+#
+export        PROTON=~/git/qpid-proton
+export          QPID=~/git/qpid
+export      DISPATCH=~/git/qpid-dispatch
+export INSTALLPREFIX=/opt/local
+
+build_log () {
+    dir=$1
+    file=${dir}/builds.txt
+    mkdir -p ${dir}
+    echo ====                               >> ${file}
+    date                                    >> ${file}
+    echo `pwd`                              >> ${file}
+    git rev-parse --symbolic-full-name HEAD >> ${file}
+    git rev-parse HEAD                      >> ${file}
+}
+
+if [ -z "$1" ]; then
+    pushd
+
+    # Flush old builds
+    rm -rf ${INSTALLPREFIX}
+    rm -rf ${PROTON}/build
+    rm -rf ${QPID}/qpid/cpp/build
+    rm -rf ${DISPATCH}/build
+    mkdir ${PROTON}/build
+    mkdir ${QPID}/qpid/cpp/build
+    mkdir ${DISPATCH}/build
+    
+    # build proton
+    cd ${PROTON}/build
+    build_log ${INSTALLPREFIX}
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${INSTALLPREFIX} ..
+    make -j 8 install
+    
+    # build qpid
+    cd ${QPID}/qpid/cpp/build
+    build_log ${INSTALLPREFIX}
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${INSTALLPREFIX} -DBUILD_DOCS=No ..
+    make -j 8 install
+
+    # build dispatch
+    cd ${DISPATCH}/build
+    build_log ${INSTALLPREFIX}
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${INSTALLPREFIX} ..
+    # Don't install. This allows running from build area.
+    #make -j 8 install
+
+    # install qpid giblets
+    cd ${QPID}/qpid/tools;  ./setup.py install --prefix ${INSTALLPREFIX}
+    cd ${QPID}/qpid/python; ./setup.py install --prefix ${INSTALLPREFIX}
+
+    popd
+fi
+
+# set up the environment
+export PATH=${PATH}:${INSTALLPREFIX}/sbin
+export PATH=${PATH}:${INSTALLPREFIX}/bin
+export PYTHONPATH=${PYTHONPATH}:${INSTALLPREFIX}/lib/proton/bindings/python
+export PYTHONPATH=${PYTHONPATH}:${INSTALLPREFIX}/lib64/proton/bindings/python
+export PYTHONPATH=${PYTHONPATH}:${INSTALLPREFIX}/lib/python2.7/site-packages
+export PYTHONPATH=${PYTHONPATH}:${INSTALLPREFIX}/lib64/python2.7/site-packages
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${INSTALLPREFIX}/lib64
